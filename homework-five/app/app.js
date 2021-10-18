708355a437b77212cad6a153c0d19131340a15a6
Tin Tin <tint8140@gmail.com>
2021-10-17 20:59:40 -0400

    homework-five

--- homework-five/app/app.js
+++ homework-five/app/app.js
@@ -1,3 +1,12 @@
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
 // function route() {
 //   let hashTag = window.location.hash;
 //   let pageID = hashTag.replace("#/", "");
